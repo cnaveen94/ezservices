b428c350f567d4c36d24aa28c52d0a0e0f50077e
cnaveen94 <cnaveen94@gmail.com>
2015-09-15 21:56:11 +0530

    With All routes

---
+++ Routes/appRoutes.js
@@ -0,0 +1,60 @@
+var express = require('express');
+
+
+var routes = function(Drink){
+
+    var appRouter = express.Router();
+
+    appRouter.route('/')
+        .get(function(req,res){
+            console.log("We are in appRouter BASE ")
+            res.send(' From Route');
+
+        });
+
+    appRouter.route('/new')
+        .get(function(req,res){
+            console.log("We are in appRouter new file")
+            var query = {};
+
+            if(req.query.genre)
+            {
+                query.genre = req.query.genre;
+            }
+            Drink.find(query, function(err,drinks){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.json(drinks);
+            });
+            //res.sendfile('data/new.json');
+
+        });
+
+    appRouter.route('/rated')
+        .get(function(req,res){
+            console.log("We are in appRouter rated file")
+            res.sendfile('data/rated.json');
+        });
+    appRouter.route('/popular')
+        .get(function(req,res){
+            console.log("We are in appRouter popular file")
+            res.sendfile('data/popular.json');
+        });
+    appRouter.route('/mixed')
+        .get(function(req,res){
+            console.log("We are in appRouter mixed file")
+            res.sendfile('data/mixed.json');
+        });
+    appRouter.route('/home')
+        .get(function(req,res){
+            console.log("We are in appRouter mixed file")
+            res.sendfile('data/home.json');
+        });
+
+    return appRouter;
+};
+
+
+
+module.exports = routes;
