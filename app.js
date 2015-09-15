81ae1e8c74b8db89669f28c1e740123138854ef8
cnaveen94 <cnaveen94@gmail.com>
2015-09-15 21:47:39 +0530

    With All routes

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-var Book = require('./models/bookModel');
+
+var db = mongoose.connect('mongodb://localhost/boozerdb');
+var Drink = require('./models/submitDrinkModel');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
-
-bookRouter = require('./Routes/bookRoutes')(Book);
-app.use('/api/books', bookRouter);
+appRouter = require('./Routes/appRoutes')(Drink);
+app.use('/api/drinks', appRouter);
 
 
 app.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World 12345 "});
+    console.log("We are in appjs router ")
+    res.json({"error1" : true,"message" : "Invalid Link"});
+    //res.sendfile('data/new.json');
 });
 
-
 app.listen(port, function(){
-    console.log('Gulp is running my app on  PORT: ' + port);
+    console.log(' My app is running on  PORT: ' + port);
 });
